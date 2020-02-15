23f604821f55f6d511f3d768723760b25d1155df
liuq36 <35245372+liuq36@users.noreply.github.com>
2020-02-14 20:04:25 -0800

    week6 exercise

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -2,6 +2,10 @@
 library("httr")
 library("jsonlite")
 
+source("apikey.R" )
+
+query_params <- list("api-key" = nyt_key)
+
 # Be sure and check the README.md for complete instructions!
 
 
@@ -10,20 +14,28 @@ library("jsonlite")
 
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- "Parasite"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
 # The resource is `reviews/search.json`
 # See the interactive console for parameter details:
 #   https://developer.nytimes.com/movie_reviews_v2.json
-#
-# You should use YOUR api key (as the `api-key` parameter)
-# and your `movie_name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+endpoint <- "reviews/search.json"
+uri <- paste0(base_uri,endpoint)
 
+query_param_list <- list("api-key" = nyt_key, "movie_name" = movie_name )
+
+
+# You should use YOUR api key (as the `api-key` parameter)
+# and your `movie_name` variable as the search query
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
+response <- GET(uri, query = query_param_list)
+body <- content(repsonse,"text")
+data <- fromJSON(body)
 
 
 # What kind of data structure did this produce? A data frame? A list?
